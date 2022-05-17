4d2f73b3efb9b460ec46025982f9938b7c7e29e8
samvancart <sam02cart@gmail.com>
2022-05-17 19:08:08 +0300

    modified urls.py

--- clicker/urls.py
+++ clicker/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('/', views.index, name='index'),
     path('<int:number>/num_clicked', views.num_clicked, name='num_clicked'),
 ]
--- num_to_bin_clicker/urls.py
+++ num_to_bin_clicker/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', include('clicker.urls')),
+    path('clicker/', include('clicker.urls')),
     path('admin/', admin.site.urls),
 ]
