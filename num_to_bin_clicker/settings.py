a256aa919634822af24863c1f649dc674e89ddad
samvancart <sam02cart@gmail.com>
2022-05-17 19:44:05 +0300

    modified settings.py for static files

--- num_to_bin_clicker/settings.py
+++ num_to_bin_clicker/settings.py
@@ -12,9 +12,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 env = environ.Env()
 
