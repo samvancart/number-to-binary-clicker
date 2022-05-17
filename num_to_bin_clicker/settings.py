a0e64162a7e11ed09628c006d057f166ef196b88
samvancart <sam02cart@gmail.com>
2022-05-17 18:30:56 +0300

    modified Procfile and Dockerfile

--- num_to_bin_clicker/settings.py
+++ num_to_bin_clicker/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DJANGO_DEBUG', default=False)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'num-to-bin-clicker.herokuapp.com']
 
 
 # Application definition
